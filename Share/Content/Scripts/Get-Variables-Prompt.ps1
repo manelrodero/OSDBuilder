311c1110874a916deb29595a7dc05ac58a751a35
Manel Rodero <manelrodero@users.noreply.github.com>
2020-10-04 21:52:38 +0200

    Script para pausar y mostrar variables (debug)

---
+++ Share/Content/Scripts/Get-Variables-Prompt.ps1
@@ -0,0 +1,12 @@
+#   v1.0 David Segura
+#   http://osdeploy.com
+#
+#   OSDBuilder Script
+#   Get-Variables.ps1
+#	Version 19.3.15
+#
+#   Displays Variables and Prompts for Enter
+#   Useful for Debugging
+#======================================================================================
+Get-Variable | Select-Object -Property Name, Value | Format-Table
+[void](Read-Host 'Press Enter to continue')
