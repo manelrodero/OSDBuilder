597ba7d46f1a8d4d039b8c2257e111c8b76d621f
Manel Rodero <manelrodero@users.noreply.github.com>
2020-10-04 22:05:00 +0200

    Script para deshabilitar la búsqueda web en la Search Bar

---
+++ Share/Content/Scripts/Windows 10 Disable-WebSearch.ps1
@@ -0,0 +1,86 @@
+#   v1.0 Manel Rodero
+#   http://www.manelrodero.com/
+#
+#   OSBuilder Script
+#   Disable-WebSearch.ps1
+
+#======================================================================================
+#   Remove Files
+#======================================================================================
+
+#======================================================================================
+#   Load Registry Hives
+#======================================================================================
+$RegDefault = "$MountDirectory\Windows\System32\Config\Default"
+if (Test-Path $RegDefault) {
+    Write-Host "Loading $RegDefault" -ForegroundColor Cyan
+    Start-Process reg -ArgumentList "load HKLM\MountDefault $RegDefault" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+$RegDefaultUser = "$MountDirectory\Users\Default\ntuser.dat"
+if (Test-Path $RegDefaultUser) {
+    Write-Host "Loading $RegDefaultUser" -ForegroundColor Cyan
+    Start-Process reg -ArgumentList "load HKLM\MountDefaultUser $RegDefaultUser" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+$RegSoftware = "$MountDirectory\Windows\System32\Config\Software"
+if (Test-Path $RegSoftware) {
+    Write-Host "Loading $RegSoftware" -ForegroundColor Cyan
+    Start-Process reg -ArgumentList "load HKLM\MountSoftware $RegSoftware" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+$RegSystem = "$MountDirectory\Windows\System32\Config\System"
+if (Test-Path $RegSystem) {
+    Write-Host "Loading $RegSystem" -ForegroundColor Cyan
+    Start-Process reg -ArgumentList "load HKLM\MountSystem $RegSystem" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+
+#======================================================================================
+#   Registry Commands
+#======================================================================================
+
+# Windows 10 - Disable Web Search in Start Menu
+# https://www.bennetrichter.de/en/tutorials/windows-10-disable-web-search/
+
+# DisableWebSearch doesn't work on 2004
+# https://github.com/Disassembler0/Win10-Initial-Setup-Script/issues/300
+
+<# Windows 10 < 1803
+$RegCommands =
+'add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v DisableWebSearch /t REG_DWORD /d 1 /f'
+#>
+
+$RegCommands =
+'add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f',
+'add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v CortanaConsent /t REG_DWORD /d 0 /f'
+
+<# Windows 10 >= 2004
+$RegCommands =
+'add "HKCU\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f'
+#>
+
+foreach ($Command in $RegCommands) {
+    if ($Command -like "*HKCU*") {
+        $Command = $Command -replace "HKCU","HKLM\MountDefaultUser"
+        Write-Host "reg $Command" -ForegroundColor Green
+        Start-Process reg -ArgumentList $Command -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+    } elseif ($Command -like "*HKLM\Software*") {
+        $Command = $Command -replace "HKLM\\Software","HKLM\MountSoftware"
+        Write-Host "reg $Command" -ForegroundColor Green
+        Start-Process reg -ArgumentList $Command -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+    } elseif ($Command -like "*HKLM\System*") {
+        $Command = $Command -replace "HKLM\\System","HKLM\MountSystem"
+        Write-Host "reg $Command" -ForegroundColor Green
+        Start-Process reg -ArgumentList $Command -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+    }
+}
+
+#======================================================================================
+#   Unload Registry Hives
+#======================================================================================
+Start-Process reg -ArgumentList "unload HKLM\MountDefault" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+Start-Process reg -ArgumentList "unload HKLM\MountDefaultUser" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+Start-Process reg -ArgumentList "unload HKLM\MountSoftware" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+Start-Process reg -ArgumentList "unload HKLM\MountSystem" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+
+#======================================================================================
+#   Testing
+#======================================================================================
+#   [void](Read-Host 'Press Enter to continue')
