d663eb901a77af82fa0aaa941b23559f39cf0b98
Manel Rodero <manelrodero@users.noreply.github.com>
2020-10-04 22:01:47 +0200

    Script para configurar zona horaria a Romance

---
+++ Share/Content/Scripts/Global Set-TimeZoneRomance.ps1
@@ -0,0 +1,19 @@
+﻿#   v1.1 Manel Rodero
+#   http://www.manelrodero.com/
+#
+#   v1.0 David Segura
+#   http://osdeploy.com
+#
+#   OSBuilder Script
+#   Set-TimeZoneRomance.ps1
+
+#======================================================================================
+if (Test-Path $MountDirectory) {
+    if (Test-Path $Logs) {
+        Dism /Image:"$MountDirectory" /Set-TimeZone:"Romance Standard Time" /LogPath:"$Logs\$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Dism-SetTimeZone.log"
+    }
+}
+#======================================================================================
+#   Testing
+#======================================================================================
+#   [void](Read-Host 'Press Enter to continue')
