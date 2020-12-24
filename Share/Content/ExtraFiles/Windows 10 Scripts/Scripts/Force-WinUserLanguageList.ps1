cddc6fe6e5c55bffb42742193b4bd2c764616e20
Manel Rodero <manelrodero@users.noreply.github.com>
2020-12-24 19:18:20 +0100

    Add URL for documentation

--- Share/Content/ExtraFiles/Windows 10 Scripts/Scripts/Force-WinUserLanguageList.ps1
+++ Share/Content/ExtraFiles/Windows 10 Scripts/Scripts/Force-WinUserLanguageList.ps1
@@ -1,5 +1,7 @@
-$prefered_list = Get-WinUserLanguageList
-$prefered_list.Add("es-es")
-$prefered_list.Add("ca-es")
-$prefered_list.Add("en-us")
-Set-WinUserLanguageList($prefered_list) -Force
+# https://patrickvandenborn.blogspot.com/2019/10/wvd-and-vdi-automation-change-in.html
+
+$preferred_list = Get-WinUserLanguageList
+$preferred_list.Add("es-es")
+$preferred_list.Add("ca-es")
+$preferred_list.Add("en-us")
+Set-WinUserLanguageList($preferred_list) -Force
