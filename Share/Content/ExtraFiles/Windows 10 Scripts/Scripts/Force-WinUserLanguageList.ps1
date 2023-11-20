7e88a21cdce0f8181219b48037aac12cd2e015c6
Manel Rodero <manelrodero@users.noreply.github.com>
2023-11-20 13:35:29 +0100

    Spanish to English

--- Share/Content/ExtraFiles/Windows 10 Scripts/Scripts/Force-WinUserLanguageList.ps1
+++ Share/Content/ExtraFiles/Windows 10 Scripts/Scripts/Force-WinUserLanguageList.ps1
@@ -1,8 +1,10 @@
 # https://patrickvandenborn.blogspot.com/2019/10/wvd-and-vdi-automation-change-in.html
 
-$preferred_list = New-WinUserLanguageList -Language 'es-ES'
+$preferred_list = New-WinUserLanguageList -Language 'en-US'
+$preferred_list.Add('es-ES')
 $preferred_list.Add('ca-ES')
-$preferred_list.Add('en-US')
-$preferred_list[2].InputMethodTips.Clear()
-$preferred_list[2].InputMethodTips.Add('0409:0000040A')
+$preferred_list[0].InputMethodTips.Clear()
+$preferred_list[0].InputMethodTips.Add('0409:0000040A')
 Set-WinUserLanguageList -LanguageList $preferred_list -Force
+Set-WinUILanguageOverride -Language 'en-US'
+Set-Culture -CultureInfo 'en-US'
